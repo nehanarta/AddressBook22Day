2fac678445edd5089c07781e716981ab81ef0a90
Dell <nehabaswantrao@gmail.com>
2023-05-04 21:47:19 +0530

    Added:Edit person Details

--- src/AddressBook/AddressBook.java
+++ src/AddressBook/AddressBook.java
@@ -31,13 +31,80 @@ public class AddressBook {
 
         Array[0] = new ContactsDetails(fName, lName, address, city, state, zip, phoneNumber, email);
     }
+    static void editContact(String name, ContactsDetails Array[]) {
+        Scanner sc = new Scanner(System.in);
+
+
+        boolean flag = false;
+        if (Array[0].firstName.equals(name)) {
+            flag = true;
+            if (flag == true) {
+                System.out.println("1.LastName 2.Address 3.City 4.State 5.ZipCode 6.PhoneNumber 7.Email 8.Exit");
+                int editOption = sc.nextInt();
+                switch (editOption) {
+                    case 1:
+                        System.out.print("Edit last name:");
+                        String tempLastName = sc.next();
+                        Array[0].lastName = tempLastName;
+                        break;
+                    case 2:
+                        System.out.print("Edit address:");
+                        String tempAddress = sc.next();
+                        Array[0].address = tempAddress;
+                        break;
+                    case 3:
+                        System.out.print("Edit city:");
+                        String tempCity = sc.next();
+                        Array[0].city = tempCity;
+                        break;
+                    case 4:
+                        System.out.print("Edit state:");
+                        String tempState = sc.next();
+                        Array[0].state = tempState;
+                        break;
+                    case 5:
+                        System.out.print("Edit zip:");
+                        int tempZip = sc.nextInt();
+                        Array[0].zip = tempZip;
+                        break;
+                    case 6:
+                        System.out.print("Edit phone:");
+                        long tempPhoneNumber = sc.nextLong();
+                        Array[0].phoneNumber = tempPhoneNumber;
+                        break;
+                    case 7:
+                        System.out.print("Edit email:");
+                        String tempEmail = sc.next();
+                        Array[0].emailId = tempEmail;
+                        break;
+                    default:
+                        System.out.println("nothing");
+                        break;
+                }
+            } else {
+                flag = false;
+            }
+        }
+        if (flag == false)
+            System.out.println(name + "not found");
+    }
+
+    static void displayContacts(ContactsDetails Array[]){
+
+        if(Array[0] == null)
+            System.out.println();
+        else
+            Array[0].displayContactDetails();
+
+        System.out.println();
+    }
     public static void main(String[]args) {
         ContactsDetails Array[] = new ContactsDetails[1];
         Scanner sc = new Scanner(System.in);
 
         int temp = 1;
         while (temp != 0) {
-            System.out.println("[1.ADD_CONTACT]");
+            System.out.println("ADD_CONTACT \\ EDIT  \\ DISPLAY ");
             System.out.print("choice:");
             int choice = sc.nextInt();
 
@@ -46,12 +113,19 @@ public class AddressBook {
                     addContact(Array);
                     break;
                 case 2:
+                    System.out.print("Edit  Contact Details:");
+                    String name = sc.next();
+                    editContact(name,Array);
+                    break;
+                case 3:
+                    displayContacts(Array);
+                    break;
+                case 4:
                     temp = 0;
                     break;
                 default:
-                    System.out.println("nothing");
-                    break;
             }
         }
-    }
-}
+            }
+        }
+
